314788e8b8aa9c4f6644f7ebad530f4216daeddb
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-04-10 00:06:35 +0530

    user types added in admin

--- userdata/admin.py
+++ userdata/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
 from .models import CustomUser
+from .forms import CustomForm
+from django.contrib.auth.admin import UserAdmin
 
 
-@admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ["username", "first_name",
-                    "last_name", "is_teacher", "is_student"]
-    list_display_links = ["username", "first_name", "last_name", "is_teacher", "is_student"]
+    add_form = CustomForm
+    list_display = ('username', 'email', 'first_name',
+                    'last_name', 'is_staff', 'is_teacher', 'is_student')
+
+    fieldsets = (
+        *UserAdmin.fieldsets, (
+            'User Types',
+            {
+                'fields': (
+                    'is_teacher',
+                    'is_student'
+                )
+            }
+        )
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
